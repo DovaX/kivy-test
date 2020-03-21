5fca48382a7566182a819e966ee2a9e1a2eb5067
Dominik Vach <vach.dominik@gmail.com>
2020-03-21 20:57:03 +0100

    chat_app

--- kivyapp.py
+++ kivyapp.py
@@ -6,13 +6,43 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.image import Image
 
 import socket_client
-#import socket_server
 import os
 import sys
 
 #kivy.require("1.10.1")
+
+
+class ScrollableLable(ScrollView):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.layout=GridLayout(cols=1, size_hint_y=None)
+        self.add_widget(self.layout)
+        
+        self.chat_history=Label(size_hint_y=None,markup=True)
+        self.scroll_to_point = Label()
+        
+        self.layout.add_widget(self.chat_history)
+        self.layout.add_widget(self.scroll_to_point)
+        
+        
+    def update_chat_history(self,message):
+        self.chat_history.text += "\n"+message
+        self.layout.height = self.chat_history.texture_size[1] + 15
+        self.chat_history.height = self.chat_history.texture_size[1]
+        self.chat_history.text_size = (self.chat_history.width*0.98,None)
+        
+        self.scroll_to(self.scroll_to_point)
+        
+    def update_chat_history_layout(self, _=None):
+        self.layout.height = self.chat_history.texture_size[1]+15
+        self.chat_history.height = self.chat_history.texture_size[1]
+        self.chat_history.text_size = (self.chat_history.width * 0.98, None)
+         
 
 class ConnectPage(GridLayout):
     def __init__(self,**kwargs):
@@ -101,9 +131,64 @@ class ChatPage(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.cols=1
-        self.add_widget(Label(text="Hey at least it worked up to this point"))
-
-
+        self.rows=2
+        
+        self.history=ScrollableLable(height = Window.size[1]*0.9, size_hint_y=None)
+        self.add_widget(self.history)
+        
+        self.new_message=TextInput(width=Window.size[0]*0.8, size_hint_x=None,multiline = False)
+        
+        self.send = Button(text = "Send")
+        self.send.bind(on_press=self.send_message)
+        
+        bottom_line = GridLayout(cols=2)
+        bottom_line.add_widget(self.new_message)
+        bottom_line.add_widget(self.send)
+        self.add_widget(bottom_line)
+        
+        Window.bind(on_key_down=self.on_key_down)
+        
+        Clock.schedule_once(self.focus_text_input, 1)
+        
+        socket_client.start_listening(self.incoming_message,show_error)
+        self.bind(size=self.adjust_fields)
+        
+    def adjust_fields(self, *_):
+        
+        if Window.size[1]* 0.1 < 50:
+            new_height = Window.size[1]-50
+        else:
+            new_height = Window.size[1]*0.9
+        self.history.height = new_height
+        
+        if Window.size[0] * 0.2 < 160:
+            new_width = Window.size[0] - 160
+        else:
+            new_width = Window.size[0] * 0.8
+        self.new_message.width = new_width
+        
+        
+        Clock.schedule_once(self.history.update_chat_history_layout, 0.001)
+        
+    def on_key_down(self,instance,keyboard,key_code, text,modifiers):
+        if key_code == 40:
+            self.send_message(None)
+        
+    def send_message(self, _):
+        message = self.new_message.text
+        self.new_message.text=""
+        if message:
+            self.history.update_chat_history(f"[color=dd2020]{chat_app.connect_page.username.text}[/color] > {message}")
+            socket_client.send(message)
+            
+        Clock.schedule_once(self.focus_text_input, 0.3)
+        
+    def focus_text_input(self, _):
+        self.new_message.focus=True
+        
+    def incoming_message(self,username,message):
+        self.history.update_chat_history(f"[color=20dd20]{username}[/color] > {message}")
+        
 class EpicApp(App):
     def build(self):
         self.screen_manager = ScreenManager()
